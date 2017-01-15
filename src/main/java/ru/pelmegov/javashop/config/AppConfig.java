a20a13c53cca664d0275dbb3c6070e9dd17fe0e2
pelmegov <pelmegovsa@gmail.com>
2017-01-15 16:09:50 +0300

    Added default-schema and updated spring security version.

--- src/main/java/ru/pelmegov/javashop/config/AppConfig.java
+++ src/main/java/ru/pelmegov/javashop/config/AppConfig.java
@@ -55,6 +55,7 @@ public class AppConfig {
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
         properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.default_schema", environment.getRequiredProperty("hibernate.default_schema"));
         return properties;
     }
 
