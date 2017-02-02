f0d16beb45bf348b7f2b053a2a4dbc9dcdad8d7b
George <geobarit@gmail.com>
2017-02-02 15:14:27 +0300

    add Category DAO and Service layers

---
+++ src/main/java/ru/pelmegov/javashop/api/service/CategoryService.java
@@ -0,0 +1,14 @@
+package ru.pelmegov.javashop.api.service;
+
+
+import ru.pelmegov.javashop.model.good.Category;
+
+public interface CategoryService {
+    Category getCategoryById(Integer id);
+
+    void addCategory(Category category);
+
+    void updateCategory(Category category);
+
+    void deleteCategoryById(Integer id);
+}
---
+++ src/main/java/ru/pelmegov/javashop/dao/CategoryDAOImpl.java
@@ -0,0 +1,48 @@
+package ru.pelmegov.javashop.dao;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.pelmegov.javashop.api.dao.CategoryDAO;
+import ru.pelmegov.javashop.model.good.Category;
+import ru.pelmegov.javashop.model.user.User;
+
+@Repository
+public class CategoryDAOImpl extends AbstractDAO implements CategoryDAO {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CategoryDAOImpl.class);
+
+    public CategoryDAOImpl(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    @Override
+    public void updateCategory(Category category){
+        getSession().update(category);
+        LOGGER.info("Category updated: ", category);
+    }
+
+    @Override
+    public void addCategory(Category category){
+        getSession().save(category);
+        LOGGER.info("Category added: ", category);
+    }
+
+    @Override
+    public Category getCategoryById(Integer id) {
+        Category category = (Category) getSession().createCriteria(Category.class).add(Restrictions.eq("id", id)).uniqueResult();
+        LOGGER.info("Category loaded: {}.", category);
+        return category;
+    }
+    @Override
+    public void deleteCategoryById(Integer id) {
+        Category category = (Category) getSession().load(Category.class, id);
+
+        if (category != null) {
+            LOGGER.info("Category removed: {}.", category);
+            getSession().delete(category);
+        }
+    }
+
+}
